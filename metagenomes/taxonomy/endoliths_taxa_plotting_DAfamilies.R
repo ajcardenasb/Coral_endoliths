6709f29be1cc1ec1a3cb7473bb459d0ea0506de7
Anny Cárdenas <ajcardenasb1@gmail.com>
2021-03-29 12:54:53 +0200

    Update endoliths_taxa_plotting_DAfamilies.R

--- metagenomes/taxonomy/endoliths_taxa_plotting_DAfamilies.R
+++ metagenomes/taxonomy/endoliths_taxa_plotting_DAfamilies.R
@@ -9,19 +9,29 @@ cnts=read.table("Input_files/Families_counts_500", sep = "\t", header = T, row.n
 cnts_clr=apply(cnts, 2, clr)
 
 tax=rbind(ancom_band, ancom_skel)
-tax=tidyr::separate(tax, Taxa, c("Superkingdom", "Phylum", "Class"), sep =";")
+#tax=tidyr::separate(tax, Taxa, c("Superkingdom", "Phylum", "Class"), sep =";")
 
 # replicated heatmaps
 clr_cnt=cnts_clr[, grepl("[GP]C[0-9][BS]", colnames(cnts_clr))]
 cnts_clr_sort=clr_cnt[, c(1,10,3,20,19,15,18,9,23,12,7,11,22,6,17,16,2,4, 24,5,14,21,8,13)]
-clr_sig=subset(cnts_clr_sort, rownames(cnts_clr_sort) %in%  rownames(subset(ancom_band, Effect_size_W > 2)) |  rownames(cnts_clr_sort) %in%  rownames(subset(ancom_skel,  Effect_size_W > 2)) ) 
-colour_taxa=data.frame(domain=tax$Superkingdom[match(rownames(clr_sig),rownames(tax))])
-rownames(clr_sig)=paste(rownames(clr_sig), tax$Class[match(rownames(clr_sig),rownames(tax))], sep = " -" )
+clr_sig=subset(cnts_clr_sort, rownames(cnts_clr_sort) %in%  rownames(subset(ancom_band, Effect_size_W > 5)) |  rownames(cnts_clr_sort) %in%  rownames(subset(ancom_skel,  Effect_size_W > 5)) ) 
+colour_taxa=data.frame(domain=tax$Domain[match(rownames(clr_sig),rownames(tax))])
+#rownames(clr_sig)=paste(rownames(clr_sig), tax$Domain[match(rownames(clr_sig),rownames(tax))], sep = " -" )
 rownames(colour_taxa)=rownames(clr_sig)
 #labels=paste(rownames(clr_sig), tax$Class[match(rownames(clr_sig),rownames(tax))], sep = " -" )
 
 pdf("outputs/heatmaps_ANCOMBC_species_taxa.pdf", height = 12, width = 9)
 pheatmap(clr_sig, color = colorRampPalette(c("#1f77b4", "white", "#f94144"))(50),  angle_col = "0",  scale = "row", cluster_row= T, cluster_col = FALSE, border_color = NA,  cellwidth = 5, cellheight =  5, fontsize_row = 6, fontsize_col= 8, legend = F,  annotation_row = colour_taxa, gaps_col = c(6,12,18), annotation_colors =  list(domain =c(Archaea="#f94144", Bacteria="#f8961e",Eukaryota="#90be6d",Viruses="#43aa8b") ))
+dev.off()
+
+# replicated heatmaps Delta and Cyano
+kai=read.table("Input_files/Endoliths_kaiju_taxonomy_500", sep = "\t", header = T, fill = T, quote = "")
+del_cyan=subset(kai, Phylum == "Cyanobacteria" | Class == "Deltaproteobacteria")$label
+clr_del_cyan=subset(cnts_clr_sort, rownames(cnts_clr_sort) %in%  del_cyan)
+colour_taxa=data.frame(domain=kai$Phylum[match(rownames(clr_del_cyan),kai$label)])
+rownames(colour_taxa)=rownames(clr_del_cyan)
+pdf("outputs/heatmaps_ANCOMBC_delta_cyano.pdf", height = 12, width = 9)
+pheatmap(clr_del_cyan, color = colorRampPalette(c("#1f77b4", "white", "#f94144"))(50),  angle_col = "0",  scale = "row", cluster_row= T, cluster_col = FALSE, border_color = NA,  cellwidth = 5, cellheight =  5, fontsize_row = 6, fontsize_col= 8, legend = F,  annotation_row = colour_taxa,  gaps_col = c(6,12,18), annotation_colors =  list(domain =c(Proteobacteria="#31AF6C", Cyanobacteria="#F38C16") ))
 dev.off()
 
 
