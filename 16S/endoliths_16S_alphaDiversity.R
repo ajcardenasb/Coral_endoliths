9ac60aeee5976d234e1d26307dbccb39b48c4ff9
Anny Cárdenas <ajcardenasb1@gmail.com>
2022-04-26 12:19:19 +0200

    Update endoliths_16S_alphaDiversity.R

--- 16S/endoliths_16S_alphaDiversity.R
+++ 16S/endoliths_16S_alphaDiversity.R
@@ -8,114 +8,68 @@ library(plyr)
 library(gridExtra)
 library(dplyr)
 library(GUniFrac)
+library(lme4)
+library(emmeans)
+
 setwd("~/Documents/Bioinformatics_scripts/R_scripts/Coral_endoliths/16S/ASVs/")
 
 asv=read.table("./outputs/ASVs_noContanoOut.raw.txt", header = TRUE, row.names = 1)
 map=read.table("./Input_files/skeleton16S-metadata.txt", header = TRUE, row.names = 1, sep ='\t')
 map$Tissue=gsub("Green band", "Endolithic band", map$Tissue)
-map$Tissue=gsub("White band", "Skeleton band", map$Tissue)
+map$Tissue=gsub("White band", "Skeleton", map$Tissue)
 
 ## aggregated to families
-asv.tax.ag=aggregate(asv[, 1:65], by = list(asv[, 70]), FUN =  sum) #define sample range and group factor
-fams=asv.tax.ag[,-1]
-rownames(fams)=asv.tax.ag$Group.1
+# asv.tax.ag=aggregate(asv[, 1:65], by = list(asv[, 70]), FUN =  sum) #define sample range and group factor
+# fams=asv.tax.ag[,-1]
+# rownames(fams)=asv.tax.ag$Group.1
 
 ##rarefying
-cnts=t(fams[, 1:65])
+cnts=t(asv[, 1:65])
 min(rowSums(cnts))
-asv.rar=Rarefy(cnts, 1966)$otu.tab.rff
-# asv.grp=merge(asv.rar, map, by="row.names")
-# rownames(asv.grp)= asv.grp[,1]
-# asv.grp=asv.grp[,-1]
+asv.rar=Rarefy(cnts, min(rowSums(cnts)))$otu.tab.rff
+#asv.grp=merge(asv.rar, map, by="row.names")
+#rownames(asv.grp)= asv.grp[,1]
+#asv.grp=asv.grp[,-1]
 
 ###not rarefying
 # cnts=t(asv[, 1:65])
+# asv.rar=cnts
 # asv.grp=merge(cnts, map, by="row.names")
 # rownames(asv.grp)= asv.grp[,1]
 # asv.grp=asv.grp[,-1]
 
 
 ###plots
-alpha=as.data.frame(t(estimateR(asv.rar,  smallsample = TRUE)))
+alpha=as.data.frame(t(estimateR(asv.rar)))
 alpha$Shannon=diversity(asv.rar, index = "shannon")#$shannon
 alpha$treatment=map$Treatment[match(rownames(alpha),rownames(map))]
 alpha$species=map$Species[match(rownames(alpha),rownames(map))]
 alpha$tissue=map$Tissue[match(rownames(alpha),rownames(map))]
-alpha$tissue=factor(alpha$tissue, levels = c("Tissue", "Endolithic band", "Skeleton band"))
+alpha$genotype=map$Genotype[match(rownames(alpha),rownames(map))]
+alpha$tissue=factor(alpha$tissue, levels = c("Tissue", "Endolithic band", "Skeleton"))
 alpha_cnts=subset(alpha, treatment == "Control")
 
 # boxplots
-pdf("./outputs/ASVs_skeleton16S_species.pdf", width=6.5,height=3, pointsize = 12)
-ggplot(alpha_cnts, aes(x=species, y=S.ACE, fill=species)) + stat_boxplot(geom = "errorbar")  + geom_boxplot(alpha = 1) +  scale_fill_manual(values=c("#8f2d56", "#218380"))  + facet_grid(~tissue) +  theme_classic() + labs( y= "Observed number of ASVs", x="") 
+pdf("./outputs/ASVs_skeleton16S_alpha_families.pdf", width=6.5,height=3, pointsize = 12)
+ggplot(alpha_cnts, aes(x=species, y=Shannon, fill=species)) + 
+  stat_boxplot(geom = "errorbar")  + geom_boxplot(alpha = 1) + 
+  scale_fill_manual(values=c("#8f2d56", "#218380"))  + facet_grid(~tissue) + 
+  theme_classic() + labs( y= "Shannon bacterial family diversity", x="") 
 dev.off()
-
-
 
 
 ##################################################
 ##################### Stats ######################
 ##################################################
 
+### full model = only controls
+alpha_cnt=subset(alpha, treatment == "Control")
+all_model=lmer(Shannon~species*tissue + (1 |genotype), data = alpha_cnt)
+anova(all_model)
+#comparing species
+all_pairs = emmeans(all_model, pairwise ~ species|tissue, weights = "proportional", adjust="none")
+rbind(all_pairs$contrasts, adjust="fdr")
 
-shapiro.test(alpha$S.ACE) # p-value > 0.05 implying we can assume the normality.
-shapiro.test(alpha$S.obs) # p-value > 0.05 implying we can assume the normality.
-
-
-### ACE
-gon.t=subset(alpha, species == "Goniastrea" & tissue =="Tissue")
-t.test(S.ACE~ treatment, gon.t )# p-value = 0.58
-gon.g=subset(alpha, species == "Goniastrea" & tissue =="Green band")
-t.test(S.ACE~ treatment,gon.g )# p-value = 0.18
-gon.w=subset(alpha, species == "Goniastrea" & tissue =="White band")
-t.test(S.ACE~ treatment,gon.w )#p-value = 0.59
-
-por.t=subset(alpha, species == "Porites" & tissue =="Tissue")
-t.test(S.ACE~ treatment, por.t )# p-value = 0.80
-por.g=subset(alpha, species == "Porites" & tissue =="Green band")
-t.test(S.ACE~ treatment,por.g )# p-value = 0.68
-por.w=subset(alpha, species == "Porites" & tissue =="White band")
-t.test(S.ACE~ treatment,por.w )#p-value = 0.72
-
-#Observed
-
-gon.t=subset(alpha, species == "Goniastrea" & tissue =="Tissue")
-t.test(S.obs~ treatment, gon.t )# p-value = 0.97
-gon.g=subset(alpha, species == "Goniastrea" & tissue =="Green band")
-t.test(S.obs~ treatment,gon.g )# p-value = 0.11
-gon.w=subset(alpha, species == "Goniastrea" & tissue =="White band")
-t.test(S.obs~ treatment,gon.w )#p-value = 0.560
-
-por.t=subset(alpha, species == "Porites" & tissue =="Tissue")
-t.test(S.obs~ treatment, por.t )# p-value = 0.69
-por.g=subset(alpha, species == "Porites" & tissue =="Green band")
-t.test(S.obs~ treatment,por.g )# p-value = 0.56
-por.w=subset(alpha, species == "Porites" & tissue =="White band")
-t.test(S.obs~ treatment,por.w )#p-value = 0.47
-
-############################################################
-#####################  Family level  ######################
-############################################################
-
-asv=read.table("./outputs/ASVs_noContanoOut.raw.txt", header = TRUE, row.names = 1)
-map=read.table("./Input_files/skeleton16S-metadata.txt", header = TRUE, row.names = 1, sep ='\t')
-map$Tissue=gsub("Green band", "Endolithic band", map$Tissue)
-map$Tissue=gsub("White band", "Skeleton band", map$Tissue)
-
-fam=aggregate(asv[, 1:65], by = list(asv[, 70]), FUN =  sum) 
-rownames(fam)= fam[,1]
-fam=fam[,-1]
-fam_t=t(fam)
-write.table(fam, "outputs/family_counts_ASVs", quote =F, sep = "\t")
-###plots
-alpha=as.data.frame(t(estimateR(asv.grp[, 1:220],  smallsample = TRUE)))
-alpha$Shannon=diversity(asv.grp[, 1:220], index = "shannon")#$shannon
-alpha$treatment=map$Treatment[match(rownames(alpha),rownames(map))]
-alpha$species=map$Species[match(rownames(alpha),rownames(map))]
-alpha$tissue=map$Tissue[match(rownames(alpha),rownames(map))]
-alpha$tissue=factor(alpha$tissue, levels = c("Tissue", "Endolithic band", "Skeleton band"))
-alpha_cnts=subset(alpha, treatment == "Control")
-
-# boxplots
-pdf("./outputs/Alpha_diversity/ASVs_skeleton16S_ObservedAlpha.pdf", width=8,height=4, pointsize = 12)
-ggplot(alpha_cnts, aes(x=species, y=Shannon, fill=species)) + stat_boxplot(geom = "errorbar")  + geom_boxplot(alpha = 1) +  scale_fill_manual(values=c("#8f2d56", "#218380"))  + facet_grid(~tissue) +  theme_classic() + labs( y= "Observed number of ASVs", x="") 
-dev.off()
+#comparing tissues
+all_pairs = emmeans(all_model, pairwise ~ tissue|species, weights = "proportional", adjust="none")
+rbind(all_pairs$contrasts, adjust="fdr")
